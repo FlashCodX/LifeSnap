61255fc1d7319bc56fe989c2c3e053742e55f422
FlashCodX <luis.pinto.trading@gmail.com>
2022-04-15 13:37:30 +0100

    Code Update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-const isProd = process.env.NODE_ENV === "production";
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  assetPrefix: isProd ? "/LifeSnap/" : "",
+  assetPrefix: !debug ? "/LifeSnap/" : "",
 };
-
-module.exports = nextConfig;
